1f1f75430491bae7b85dc5ec3a898a8a1e89bb76
Ale Breuer <ale.breuer14@gmail.com>
2022-10-27 23:23:20 -0300

    Entrega completa

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,16 +3,16 @@ import CartWidget from "../components/CartWidget";
 
 const NavBar = () => {
     return (
-        <div >
+        <div>
             
             <ul class="menu1">
                 <li><a class="nav-link" href="#"><b>Inicio</b></a></li>
                 <li><a class="nav-link" href="#"><b>Novedades</b></a></li>
                 <li><a class="nav-link" href="#"><b>Contacto</b></a></li>
-                <img src={logo} alt="" class="logo"></img>
             </ul>
+            <img src={logo} alt="" class="logo"></img>
             <ul class="menu2">
-            <li><a class="nav-link" href="#"><b>Ubicacion</b></a></li>
+                <li><a class="nav-link" href="#"><b>Ubicacion</b></a></li>
                 <li><a class="nav-link" href="#"><b>Productos</b></a></li>
                 <li><a class="nav-link" href="#"><b>Nosotras</b></a></li>
             </ul>
